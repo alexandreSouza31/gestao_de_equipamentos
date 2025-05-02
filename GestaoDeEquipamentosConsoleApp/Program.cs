1f0d0f264347da9e17550623790048d0e8634292
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-05-02 16:20:04 -0300

    criará o método de cadastrar equipamento

--- GestaoDeEquipamentosConsoleApp/Program.cs
+++ GestaoDeEquipamentosConsoleApp/Program.cs
@@ -6,14 +6,76 @@ namespace GestaoDeEquipamentosConsoleApp
     {
         static void Main(string[] args)
         {
-            Equipamento equipamento=new Equipamento();
+            TelaEquipamento telaEquipamento = new TelaEquipamento();
+            while (true)
+            {
+                char opcaoEscolhida = telaEquipamento.ApresentarMenu();
+
+                if (opcaoEscolhida == 's')
+                {
+                    break;
+                }
+
+                switch(opcaoEscolhida)
+                {
+                    case '1':
+                        telaEquipamento.Cadastrar();
+                        break;
+                }
+            }
         }
     }
 
-    internal class Equipamento
+    public class TelaEquipamento
     {
-        public int id;
-        public string nome;//nome.length>6
+        public char ApresentarMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("----- Gestão de Equipamentos -----");
+            Console.WriteLine(); Console.WriteLine("1 - Cadastrar Equipamento");
+            Console.WriteLine("S - Sair");
+            Console.Write("\nDigite uma opçpão: ");
+            char opcaoEscolhida= Convert.ToChar(Console.ReadLine()!.ToUpper()[0]);
+            Console.Write("Digite [Enter] para continuar ");
+            Console.ReadLine();
+
+            return opcaoEscolhida;
+        }
+
+        public void Cadastrar()
+        {
+            Equipamento equipamento = new Equipamento();
+
+            Console.Clear();
+            Console.WriteLine("----- Gestão de Equipamentos -----");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("-- Cadastrar Equipamento --");
+            Console.WriteLine();
+            equipamento.id = equipamento.id++;
+
+            Console.Write("Nome: ");
+            equipamento.nome = Console.ReadLine()!;
+            Console.Write("Preço de Aquisicao: ");
+            equipamento.precoAquisicao = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Numero de Série: ");
+            equipamento.numeroSerie = Console.ReadLine()!;
+            Console.Write("Data de Fabricação ex:[05/08/2022]: ");
+            equipamento.dataFabricacao = DateTime.Parse(Console.ReadLine()!);
+            Console.Write("Fabricante: ");
+            equipamento.fabricante = Console.ReadLine()!;
+
+            Console.WriteLine($"nome: {equipamento.nome} cadastrado com sucesso! id: {equipamento.id}")
+            Console.ReadLine();
+
+        }
+    }
+
+    public class Equipamento
+    {
+        static int numeroId = 1;
+        public int id = numeroId++;
+        public string nome;
         public decimal precoAquisicao;
         public string numeroSerie;
         public DateTime dataFabricacao;
