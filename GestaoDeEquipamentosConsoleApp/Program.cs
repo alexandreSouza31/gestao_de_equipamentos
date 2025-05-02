b82b6fa6f7c7df104d2d99811c0cc8f35dce4935
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-05-02 11:17:55 -0300

    Add project files.

---
+++ GestaoDeEquipamentosConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace GestaoDeEquipamentosConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Equipamento equipamento=new Equipamento();
+        }
+    }
+
+    internal class Equipamento
+    {
+        public int id;
+        public string nome;//nome.length>6
+        public decimal precoAquisicao;
+        public string numeroSerie;
+        public DateTime dataFabricacao;
+        public string fabricante;
+    }
+}
