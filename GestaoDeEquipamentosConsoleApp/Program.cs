81d12751aaaeae36968edc16e277af2489f63d11
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-05-05 14:15:00 -0300

    ajustará a lógica para sair do app

--- GestaoDeEquipamentosConsoleApp/Program.cs
+++ GestaoDeEquipamentosConsoleApp/Program.cs
@@ -11,7 +11,7 @@ namespace GestaoDeEquipamentosConsoleApp
             {
                 char opcaoEscolhida = telaEquipamento.ApresentarMenu();
 
-                if (opcaoEscolhida == 's')
+                if (opcaoEscolhida == 'S')
                 {
                     break;
                 }
