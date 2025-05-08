160ca33fc365ff8e25d6a92b5d2b73cb76f083db
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-05-08 18:01:03 -0300

    criará a classe fabricante com seus atributos

---
+++ GestaoDeEquipamentosConsoleApp/Negocio/Fabricante.cs
@@ -0,0 +1,10 @@
+﻿namespace GestaoDeEquipamentosConsoleApp.Negocio
+{
+    public class Fabricante
+    {
+        public int id;
+        public string nome;
+        public string email;
+        public string telefone;
+    }
+}
